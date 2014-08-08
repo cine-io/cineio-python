c3c8ccfbb812ebdd2937adb225601cfd129cd12b
Thomas Shafer <thomasjshafer@gmail.com>
2014-08-08 11:35:22 -0700

    moved __version__ into it's own file

--- cine_io/__init__.py
+++ cine_io/__init__.py
@@ -11,10 +11,10 @@ See http://cine.io/ for documentation.
 
 from .cine_io import Client, Project, Stream, StreamRecording, requests
 
+from .version import __version__
 __all__ = [Client, Project, Stream, StreamRecording, requests]
 __author__ = 'cine.io engineering'
 __copyright__ = 'Copyright 2014 cine.io'
 __license__ = 'MIT'
 __title__ = 'cineio'
-__version__ = '0.1.0'
 __version_info__ = tuple(int(i) for i in __version__.split('.'))
